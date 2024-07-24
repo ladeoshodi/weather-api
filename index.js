f956943d313d0df3708b8116ae0297a553eaa3ce
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-07-24 11:46:22 +0100

    Display current weather condition card

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const WEATHER_API_KEY = "LVP2BAZE38STTXRBX55CEXHLE";
+const weatherToday = document.querySelector(".weather-today");
+const weatherHourly = document.querySelector(".weather-hourly");
+const weatherDaily = document.querySelector(".weather-daily");
 
 async function getWeatherReport(location) {
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${WEATHER_API_KEY}`;
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${WEATHER_API_KEY}`;
 
   try {
     const response = await fetch(url);
@@ -9,10 +12,47 @@ async function getWeatherReport(location) {
       throw new Error(`Response status: ${response.status}`);
     }
     const data = await response.json();
-    console.log(data);
+    return data;
   } catch (e) {
     console.error(e.message);
   }
 }
 
-// getWeatherReport("london");
+function updateCurrentWeatherDisplay(data) {
+  const weatherCurrent = document.querySelector(".weather-current");
+
+  // create current weather display location & time
+  const weatherCurrentInfo = document.createElement("div");
+  weatherCurrentInfo.classList.add("weather-current-info");
+  weatherCurrentInfo.textContent = `${data.resolvedAddress} As of ${data.currentConditions.datetime} ${data.timezone}`;
+
+  // create current temperature display
+  const weatherCurrentTemp = document.createElement("div");
+  weatherCurrentTemp.classList.add("weather-current-temp");
+  weatherCurrentTemp.textContent = `${data.currentConditions.temp} ℃`;
+
+  // create current conditions display
+  const weatherCurrentConditions = document.createElement("div");
+  weatherCurrentConditions.classList.add("weather-current-condition");
+  weatherCurrentConditions.textContent = `${data.currentConditions.conditions}`;
+
+  // create current conditions description
+  const weatherCurrentDescription = document.createElement("div");
+  weatherCurrentDescription.classList.add("weather-current-description");
+  weatherCurrentDescription.textContent = `${data.description}`;
+
+  // create current conditions icon
+  const weatherCurrentIcon = document.createElement("img");
+  weatherCurrentIcon.classList.add("weather-current-icon");
+  weatherCurrentIcon.src = `weather-icons/${data.currentConditions.icon}.svg`;
+
+  weatherCurrent.appendChild(weatherCurrentInfo);
+  weatherCurrent.appendChild(weatherCurrentTemp);
+  weatherCurrent.appendChild(weatherCurrentConditions);
+  weatherCurrent.appendChild(weatherCurrentIcon);
+  weatherCurrent.appendChild(weatherCurrentDescription);
+}
+
+getWeatherReport("london").then((data) => {
+  updateCurrentWeatherDisplay(data);
+});
